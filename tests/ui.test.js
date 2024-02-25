f7ff63c972118df5291ab20f018fad7f232a573e
Ivan Lichkov <Vanko.lichkov@icloud.com>
2024-02-25 10:47:15 +0200

    Register button test

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,7 +1,9 @@
 const {expect, test} = require("@playwright/test");
+const pageUrl = "http://localhost:5500";
+
 
 test('Verify "All Books" link is vissible', async ({page}) => {
-    await page.goto("http://localhost:5500")
+    await page.goto(pageUrl)
     await page.waitForSelector('nav.navbar')
     const allBooksLink = await page.$('a[href="/catalog"]')
 
@@ -11,11 +13,21 @@ test('Verify "All Books" link is vissible', async ({page}) => {
 });
 
 test('Verify Login Button is vissible', async ({page}) => {
-    await page.goto("http://localhost:5500")
+    await page.goto(pageUrl)
     await page.waitForSelector('a[href="/login"]')
     const loginButton = await page.$('a[href="/login"]')
 
     const loginButtonIsVissible = await loginButton.isVisible();
 
     expect(loginButtonIsVissible).toBe(true);
+});
+
+test('Verify Register Button is vissible', async ({page}) => {
+    await page.goto(pageUrl)
+    await page.waitForSelector('a[href="/register"]')
+    const registerButton = await page.$('a[href="/register"]')
+
+    const registerButtonIsVissible = await registerButton.isVisible();
+
+    expect(registerButtonIsVissible).toBe(true);
 });
