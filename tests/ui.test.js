dc904cdda7961fdc6b0b1b9789ffccffc61e0b85
Ivan Lichkov <Vanko.lichkov@icloud.com>
2024-02-25 10:39:36 +0200

    login button visible test

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -9,3 +9,13 @@ test('Verify "All Books" link is vissible', async ({page}) => {
 
     expect(isAllBooksLinkVisible).toBe(true);
 });
+
+test('Verify Login Button is vissible', async ({page}) => {
+    await page.goto("http://localhost:5500")
+    await page.waitForSelector('a[href="/login"]')
+    const loginButton = await page.$('a[href="/login"]')
+
+    const loginButtonIsVissible = await loginButton.isVisible();
+
+    expect(loginButtonIsVissible).toBe(true);
+});
